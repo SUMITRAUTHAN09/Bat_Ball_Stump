78a76cb95bc58cf39fc14556317f9f0328bc0d9f
SUMIT RAUTHAN <145665661+SUMITRAUTHAN09@users.noreply.github.com>
2025-09-02 13:06:34 +0530

    Update cricket.js
    commit that edit

--- cricket.js
+++ cricket.js
@@ -1,3 +1,10 @@
+let score={
+  win:0,
+  lost:0,
+  tie:0,
+}
+
+
 function generateComputerChoice(){
     let randomNumber=Math.random()*3;
     computerChoice='';
@@ -15,15 +22,30 @@ function generateComputerChoice(){
       let computerMove=generateComputerChoice();
       let resultMsg='';
       if(userMove=== computerMove){
-        resultMsg='Tie';
+        score.tie++;
+        esultMsg='Tie';
       }
       else if(userMove==='Bat' && computerMove==='Ball' || userMove==='Ball' && computerMove==='Stump' || userMove==='Stump' && computerMove==='Bat'){
+        score.win++;
         resultMsg='You Win';
       }
       else{
+        score.lost++; 
         resultMsg='you Lose';
       }
         document.querySelector(`#ComputerChoice`).value=computerMove;
         document.querySelector(`#UserChoice`).value=userMove;
         document.querySelector(`#result`).value=resultMsg;  
+        document.querySelector(`#score`).value=`win: ${score.win} lost: ${score.lost} tie: ${score.tie}`;
+        console.log(score);
       }
+    
+    function reset(){
+      score.win=0;
+      score.lost=0;
+      score.tie=0;
+      document.querySelector(`#ComputerChoice`).value='';
+      document.querySelector(`#UserChoice`).value='';
+      document.querySelector(`#result`).value='';  
+      document.querySelector(`#score`).value=`win: ${score.win} lost: ${score.lost} tie: ${score.tie}`;
+    }
